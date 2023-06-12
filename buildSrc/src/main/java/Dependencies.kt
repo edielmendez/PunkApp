972fa7f8bb21f431b87a0ea77176e17d9c59f6cf
Dev MV <developer@mundoweb.com.mx>
2023-06-12 13:19:24 -0600

    Add Kotlin DSL

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,9 @@
+object Dependencies {
+    val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val material = "com.google.android.material:material:${Versions.material}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    val jUnit = "junit:junit:${Versions.appCompat}"
+    val extJunit = "androidx.test.ext:junit:${Versions.appCompat}"
+    val espressoCore = "androidx.test.espresso:espresso-core:${Versions.appCompat}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    const val coreKtx = "1.7.0"
+    const val appCompat = "1.6.1"
+    const val material = "1.9.0"
+    const val constraintLayout = "2.1.4"
+    const val jUnit = "4.13.2"
+    const val extJunit = "1.1.5"
+    const val espressoCore = "3.5.1"
+}
