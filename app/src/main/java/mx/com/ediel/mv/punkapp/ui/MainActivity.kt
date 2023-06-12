67e6db8dde18b1b4c051566b8bdc3030ea3d54a7
Dev MV <developer@mundoweb.com.mx>
2023-06-12 13:58:01 -0600

    Add packages structure

--- app/src/main/java/mx/com/ediel/mv/punkapp/ui/MainActivity.kt
+++ app/src/main/java/mx/com/ediel/mv/punkapp/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package mx.com.ediel.mv.punkapp
+package mx.com.ediel.mv.punkapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.com.ediel.mv.punkapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
     }
 }
